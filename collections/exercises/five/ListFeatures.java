cfb8859d7dbf99586b087e5374eed20a45fa280c
PostMaster Pozitron <pmpozitron@gmail.com>
2022-11-14 18:37:43 +0300

    5th, 6th, 7th exercises (5th and 6th on 'minimal')

---
+++ collections/exercises/five/ListFeatures.java
@@ -0,0 +1,72 @@
+package exercises.five;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class ListFeatures {
+    public static void main(String[] args) {
+        Random rnd = new Random(47);
+        List<Integer> integers = Generator.integers(rnd);
+        System.out.println(integers);
+        Integer seventySeven = 77;
+        integers.add(seventySeven);
+        System.out.println(integers);
+        System.out.println(integers.contains(seventySeven));
+        integers.remove(seventySeven);
+        System.out.println(integers.indexOf(seventySeven));
+        List<Integer> sublist = integers.subList(0, 5);
+        System.out.println(integers.containsAll(sublist));
+        List<Integer> combined = new ArrayList<>(integers);
+        Integer[] arr = new Integer[]{};
+        sublist.toArray(arr);
+        Collections.addAll(combined, new Integer[]{});
+        System.out.println(combined);
+        System.out.println(combined.containsAll(integers));
+        System.out.println(combined.containsAll(sublist));
+        Collections.shuffle(integers);
+        System.out.println(integers);
+        Collections.sort(integers);
+        integers.remove(Integer.valueOf(61));
+        System.out.println(integers);
+
+    }
+}
+
+class Generator {
+    static List<Integer> integers(Random rnd) {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> rnd.nextInt(100))
+                .collect(Collectors.toList());
+    }
+
+    static List<Double> doubles(Random rnd) {
+        return rnd.doubles(10)
+                .mapToObj(Double::valueOf)
+                .collect(Collectors.toList());
+
+    }
+
+    static List<Long> longs(Random rnd) {
+        return rnd.longs(10)
+                .mapToObj(Long::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    static List<String> strings(Random rnd) {
+        List<Character> alphabet = Arrays.asList(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g'
+        );
+        List<String> result = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            StringBuilder string = new StringBuilder();
+            int bound = rnd.nextInt(10);
+            for (int j = 0; j < bound; j++) {
+                string.append(alphabet.get(rnd.nextInt(alphabet.size())));
+            }
+            result.add(string.toString());
+        }
+        return result;
+    }
+}
---
+++ collections/exercises/seven/Seven.java
@@ -0,0 +1,54 @@
+package exercises.seven;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Seven {
+    private static int COUNTER = 0;
+
+    private static Seven[] INSTANCES = new Seven[]{new Seven(), new Seven(), new Seven(), new Seven(), new Seven(), new Seven(), new Seven()};
+    private int id = COUNTER++;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seven)) return false;
+        Seven seven = (Seven) o;
+        return id == seven.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Seven.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(INSTANCES));
+
+        List<Seven> theList = new ArrayList<>(INSTANCES.length);
+        Collections.addAll(theList, INSTANCES);
+        System.out.println(theList);
+        Collections.shuffle(theList);
+        System.out.println(theList);
+        System.out.println(Arrays.toString(INSTANCES));
+        List<Seven> sub = theList.subList(0, 3);
+        System.out.println(sub);
+        List<Seven> copy = new ArrayList<>(sub);
+        theList.removeAll(sub);
+        System.out.println(theList);
+        System.out.println(copy);
+
+    }
+}
